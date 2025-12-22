bd9cff49ffc945acfac8852630d1f2a5c2907f17
Ali Al-Thaqbani <abu7alwah@gmail.com>
2025-12-22 22:21:15 +0300

    Add ensureUsersFile function to initialize users.txt if it doesn't exist

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+using namespace std;
 
 void ClearScreen()
 {
     system("cls");
+}
+
+void ensureUsersFile()
+{
+    ifstream fin("users.txt");
+    if (!fin)
+    {
+        ofstream fout("users.txt");
+        if (fout)
+        {
+            fout << "admin|123|admin\n";
+            fout << "user|456|user\n";
+            fout.close();
+        }
+    }
+    else
+        fin.close();
 }
 
 int main()
